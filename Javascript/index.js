ec5ef3fa07b83f313c64b840644a015ba550b441
PhaniChunduri <nagaphanichaitanya@gmail.com>
2024-12-09 10:47:22 +0530

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,40 @@
+// console.log('Hello from index.js');
+
+// // Variables
+// let age = 26;
+// console.log(age);
+// // const name ='Phani';
+// // console.log(name);
+
+// Data Types:
+
+// const name = 'Phani'
+// const language = 'JS'
+// const channel = 'Codevalution'
+
+// const total = 0
+// const PI = 3.14
+
+// const isPrimary = true
+// const isNewuser = false
+
+// const result = null
+// console.log(result)
+
+// const isOdd = 15%3===0 ? true : false;
+// console.log(isOdd);
+
+// console.log('Phani' / 2);
+
+// let sum = 42;
+// const max = String(sum);
+// console.log(typeof(max));
+
+// console.log(Boolean(10)); // If we convert null, 0 ,'',Nan to Boolean will get false as an output.
+
+// const var1 = NaN;
+// const var2 = 'NaN';
+// console.log(var1==var2);
+// console.log(var1===var2);
+
+
